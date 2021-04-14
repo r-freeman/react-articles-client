849332dfb767bb46e2f071a24fe62cb856286d8b
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-14 16:03:50 +0100

    Added react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
 import store from './redux/store';
 import App from './App';
 import './index.css';
@@ -8,7 +9,9 @@ import './index.css';
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App/>
+            <Router>
+                <App/>
+            </Router>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
