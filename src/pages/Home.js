001f58815b14cbe9ca59cf1a88b38a7852d3a24e
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-16 20:30:38 +0100

    Updated Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,11 @@ import ArticleList from '../components/ArticleList';
 function Home() {
     return (
         <div>
-            <Nav/>
-            <div className="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-                <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
+            <header>
+                <Nav/>
+            </header>
+            <div className="bg-white pt-16 pb-20 lg:pt-24 lg:pb-28 px-8">
+                <div className="relative max-w-full mx-auto divide-y-2 divide-gray-200">
                     <div>
                         <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Latest</h2>
                         <div className="mt-3 sm:mt-4">
