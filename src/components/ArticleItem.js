aef6815f09e1b87f80a74c2f455fee43f18e3fea
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-16 20:29:59 +0100

    Updated ArticleItem and ArticleList components

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -6,21 +6,26 @@ function ArticleItem({article}) {
 
     return (
         <div>
-            <p className="text-sm text-gray-500">
-                <time
-                    dateTime={new Date().toLocaleDateString(undefined, dateOptions)}>
-                    {new Date().toLocaleDateString(undefined, dateOptions)}
-                </time>
-            </p>
-            <a href="#" className="mt-2 block">
+            <NavLink to={`articles/${article.slug}`} className="mt-2 block">
                 <p className="text-xl font-semibold text-gray-900">{article.title}</p>
-                <p className="mt-3 text-base text-gray-500">{article.body}</p>
-            </a>
-            <div className="mt-3">
-                <NavLink to=""
-                         className="text-base font-semibold text-indigo-600 hover:text-indigo-500">
-                    Read full story
-                </NavLink>
+                <p className="mt-3 text-base text-gray-500">{article.excerpt}</p>
+            </NavLink>
+            <div className="mt-6 flex items-center">
+                <div className="flex-shrink-0">
+                    <NavLink to={`articles/${article.slug}`}>
+                        <span className="sr-only">{article.author.name}</span>
+                        <img className="h-10 w-10 rounded-full" src={article.author.photo} alt=""/>
+                    </NavLink>
+                </div>
+                <div className="ml-3">
+                    <p className="text-sm font-medium text-gray-900">
+                        <NavLink to={`articles/${article.slug}`}>{article.author.name}</NavLink>
+                    </p>
+                    <div className="flex space-x-1 text-sm text-gray-500">
+                        <time
+                            dateTime={new Date().toLocaleDateString(undefined, dateOptions)}>{new Date().toLocaleDateString(undefined, dateOptions)}</time>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,14 +4,29 @@ import ArticleItem from './ArticleItem';
 
 function ArticleList() {
     const {articles} = useSelector(state => state.articles);
+    const hasArticles = articles.length > 0;
 
     return (
-        <div className="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
-            {articles.map(article => (
-                <ArticleItem article={article} key={article._id}/>
-            ))}
-        </div>
-    );
+        <React.Fragment>
+            {hasArticles ?
+                <div className="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
+                    {articles.map(article => (
+                        <ArticleItem article={article} key={article._id}/>
+                    ))}
+                </div>
+                :
+                <div className="mt-6 pt-10">
+                    <svg className="animate-spin mx-auto h-10 w-10 text-indigo-500" xmlns="http://www.w3.org/2000/svg"
+                         fill="none"
+                         viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"/>
+                        <path className="opacity-75" fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                    </svg>
+                </div>
+            }
+        </React.Fragment>
+    )
 }
 
 export default ArticleList;
