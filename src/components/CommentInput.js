e3ed7d36ae932c31fa268328fcdc46df3240d645
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-23 21:21:40 +0100

    Added animation when posting comment

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -5,6 +5,7 @@ import actions from '../redux/actions';
 
 function CommentInput() {
     const {user} = useSelector(state => state.auth);
+    const {isPostingComment} = useSelector(state => state.articles);
     const dispatch = useDispatch();
     const commentInput = useRef();
 
@@ -36,9 +37,18 @@ function CommentInput() {
                         </div>
                         <div className="mt-3 flex justify-end">
                             <button type="submit"
-                                    className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    className="w-full sm:w-36 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                     onClick={handleCommentSubmit}>
-                                Post comment
+                                {isPostingComment
+                                    ? <svg className="animate-spin inline-flex h-5 w-5 text-white"
+                                           xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle className="opacity-25" cx="12" cy="12" r="10"
+                                                stroke="currentColor" strokeWidth="4"/>
+                                        <path className="opacity-75" fill="currentColor"
+                                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                                    </svg>
+                                    : <span className="h-5">Post comment</span>
+                                }
                             </button>
                         </div>
                     </form>
