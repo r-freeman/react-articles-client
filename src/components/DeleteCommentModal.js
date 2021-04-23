229423a2f6321a12c4517f6a4bcd8a2c681e02de
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-23 22:39:09 +0100

    Added animation to delete button in DeleteCommentModal

--- src/components/DeleteCommentModal.js
+++ src/components/DeleteCommentModal.js
@@ -4,11 +4,11 @@ import {useDispatch, useSelector} from 'react-redux';
 import actions from '../redux/actions';
 
 function DeleteCommentModal({toggleDeleteCommentModal}) {
-    const {currentComment} = useSelector(state => state.articles);
+    const {currentComment, isDeletingComment} = useSelector(state => state.articles);
     const dispatch = useDispatch();
 
-    const handleDeleteComment = () => {
-        dispatch(actions.articles.deleteComment(currentComment));
+    const handleDeleteComment = async () => {
+        await dispatch(actions.articles.deleteComment(currentComment));
         toggleDeleteCommentModal();
     };
 
@@ -42,12 +42,22 @@ function DeleteCommentModal({toggleDeleteCommentModal}) {
                     </div>
                     <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
                         <button type="button"
-                                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                onClick={handleDeleteComment}>
-                            Delete
+                                className="w-full sm:w-20 inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:text-sm"
+                                onClick={handleDeleteComment}
+                                disabled={isDeletingComment}>
+                            {isDeletingComment
+                                ? <svg className="animate-spin inline-flex h-5 w-5 text-white"
+                                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10"
+                                            stroke="currentColor" strokeWidth="4"/>
+                                    <path className="opacity-75" fill="currentColor"
+                                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                                </svg>
+                                : <span className="h-5">Delete</span>
+                            }
                         </button>
                         <button type="button"
-                                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                                className="mt-3 w-full sm:w-20 inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:text-sm"
                                 onClick={toggleDeleteCommentModal}
                         >
                             Cancel
