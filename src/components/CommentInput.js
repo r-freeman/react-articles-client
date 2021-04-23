a7529277c82e663fc340139c39e71faa10e8df2d
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-23 21:58:07 +0100

    Added form validation to CommentInput component with formik

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -1,5 +1,6 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {useFormik} from 'formik';
 
 import actions from '../redux/actions';
 
@@ -7,14 +8,27 @@ function CommentInput() {
     const {user} = useSelector(state => state.auth);
     const {isPostingComment} = useSelector(state => state.articles);
     const dispatch = useDispatch();
-    const commentInput = useRef();
 
-    async function handleCommentSubmit(e) {
-        e.preventDefault();
+    const validate = values => {
+        const errors = {};
 
-        const comment = commentInput.current.value;
-        await dispatch(actions.articles.postComment(comment));
+        if (!values.comment) {
+            errors.comment = 'Comment is required.';
+        }
+
+        return errors;
     }
+
+    const formik = useFormik({
+        initialValues: {
+            comment: ''
+        },
+        validate,
+        onSubmit: async values => {
+            formik.resetForm();
+            await dispatch(actions.articles.postComment(values.comment));
+        }
+    });
 
     return (
         <div className="mt-8">
@@ -30,15 +44,33 @@ function CommentInput() {
                     <form action="#">
                         <div>
                             <label htmlFor="comment" className="sr-only">Write a comment.</label>
-                            <textarea id="comment" name="comment" rows="5"
-                                      className="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border-gray-300 bg-gray-50 rounded-md"
+                            <textarea id="comment"
+                                      name="comment"
+                                      rows="5"
+                                      className={`${formik.errors.comment ? 'border-red-500' : 'border-gray-300'} shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500  sm:text-sm  bg-gray-50 rounded-md`}
                                       placeholder="Write a comment."
-                                      ref={commentInput}/>
+                                      onChange={formik.handleChange}
+                                      value={formik.values.comment}
+                            />
                         </div>
-                        <div className="mt-3 flex justify-end">
+                        <div
+                            className={`${formik.errors.comment ? 'justify-between' : 'justify-end'} mt-3 flex items-center`}>
+                            {formik.errors.comment &&
+                            <div className="inline-flex items-center text-red-500">
+                                <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor"
+                                     viewBox="0 0 24 24"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                          d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                </svg>
+                                <p className="text-sm font-medium">{formik.errors.comment}</p>
+                            </div>
+                            }
                             <button type="submit"
-                                    className="w-full sm:w-36 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    onClick={handleCommentSubmit}>
+                                    className="w-36 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    onClick={formik.handleSubmit}
+                                    disabled={isPostingComment}
+                            >
                                 {isPostingComment
                                     ? <svg className="animate-spin inline-flex h-5 w-5 text-white"
                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
