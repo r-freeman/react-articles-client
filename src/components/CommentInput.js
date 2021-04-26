cf2d8fdc0a498b5b7afaf9de3344f49a114558b0
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-26 01:48:06 +0100

    Updated CommentInput

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -67,7 +67,7 @@ function CommentInput() {
                             </div>
                             }
                             <button type="submit"
-                                    className="w-36 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    className="w-36 inline-flex items-center justify-center px-4 py-2 border border-transparent leading-tight text-base sm:text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                     onClick={formik.handleSubmit}
                                     disabled={isPostingComment}
                             >
@@ -79,7 +79,7 @@ function CommentInput() {
                                         <path className="opacity-75" fill="currentColor"
                                               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
                                     </svg>
-                                    : <span className="h-5">Post comment</span>
+                                    : <span className="h-5">Submit</span>
                                 }
                             </button>
                         </div>
