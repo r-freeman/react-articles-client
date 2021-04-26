80b3919440f51811f0b98a1099ae98fcff8ad7d9
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-26 21:44:08 +0100

    Updated Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -124,7 +124,7 @@ function Nav() {
                             </NavLink>
                             <NavLink
                                 exact
-                                to="/register"
+                                to="/signup"
                                 as="button"
                                 type="button"
                                 className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">
