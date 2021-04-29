1044e95ba96e9667b9200cfa2252697085436424
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-29 15:51:35 +0100

    Cleaned up imports

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import ArticleFilter from './ArticleFilter';
+import ArticleItem from './ArticleItem';
+import ArticleList from './ArticleList';
+import CommentInput from './CommentInput';
+import CommentItem from './CommentItem';
+import CommentList from './CommentList';
+import DeleteArticleModal from './DeleteArticleModal';
+import DeleteCommentModal from './DeleteCommentModal';
+import UpdateCommentModal from './UpdateCommentModal';
+import Nav from './Nav';
+
+export {
+    ArticleFilter,
+    ArticleItem,
+    ArticleList,
+    CommentInput,
+    CommentItem,
+    CommentList,
+    DeleteArticleModal,
+    DeleteCommentModal,
+    UpdateCommentModal,
+    Nav
+};
